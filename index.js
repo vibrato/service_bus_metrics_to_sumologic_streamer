efd8cdb58a6114a3fd25c468dae62f845de6451b
Ray Ng <ray.ng@vibrato.com.au>
2018-07-10 17:05:37 +1000

    wrap sbService.listQueues into a function

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const azure = require('azure');
 const _ = require("lodash");
 
+function checkForQueues(sbService, qName) {
+  sbService.listQueues((error, listqueuesresult, response) => {
+     var filteredQueue = _.find(listqueuesresult,{QueueName: qName})
+     var result = _.assign(filteredQueue.CountDetails, { 'QueueName': filteredQueue.QueueName })
+     console.log(result)
+  });
+}
+
 const connStr = process.env.CONNECTION_STRING;
-const qName = process.env.QUEUE_NAME
 if (!connStr) throw new Exception('Must provide connection string');
 
+const qName = process.env.QUEUE_NAME
 const sbService = azure.createServiceBusService(connStr);
-
-sbService.listQueues((error, listqueuesresult, response) => {
-   var filteredQueue = _.find(listqueuesresult,{QueueName: qName})
-   var result = _.assign(filteredQueue.CountDetails, { QueueName: filteredQueue.QueueName })
-   console.log(result)
-   //console.log(filteredQueue[0].QueueName)
-});
+setInterval(checkForQueues.bind(null,sbService, qName),5000);
